229be64e7af34c705894c0b3b2f7d029b7a63daa
Prathamesh Patil <prathameshp@thoughtsketch.com>
2024-10-11 17:03:55 +0530

    added_employeeList_button

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -5,7 +5,12 @@ sap.ui.define([
 	return BaseController.extend("sap.ui.demo.nav.controller.Home", {
 		onDisplayNotFound : function () {
 			//display the "notFound" target without changing the hash
-			this.getRouter().getTargets().display("notFound");
+			this.getRouter().getTargets().display("notFound", {
+				fromTarget : "home"
+			});
+		},
+		onNavToEmployees : function (){
+			this.getRouter().navTo("employeeList");
 		}
 	});
 });
